98449eb6604847af89ca6b21a13edd6a2fe6c480
fnamayi <franklynenm@gmail.com>
2026-03-10 12:00:45 +0300

    Establish RabbitMQ connection and declare queue
    - Create PlainCredentials with RabbitMQ user and password
    - Establish BlockingConnection to RabbitMQ server
    - Get channel and declare durable queue

--- srcs/billing-app/app/consume_queue.py
+++ srcs/billing-app/app/consume_queue.py
@@ -10,3 +10,17 @@ RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
 RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
 RABBITMQ_PORT = os.getenv('RABBITMQ_PORT')
 RABBITMQ_QUEUE = os.getenv('RABBITMQ_QUEUE')
+
+
+def consume_and_store_order(engine):
+    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            RABBITMQ_HOST,
+            RABBITMQ_PORT,
+            '/',
+            credentials
+        )
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
